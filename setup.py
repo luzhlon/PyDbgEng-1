1036a7c787b42b2d1a4911916724557ea184378c
走神™ <walkerfuz@users.noreply.github.com>
2017-12-29 13:12:49 +0800

    change version to 0.0.5

--- setup.py
+++ setup.py
@@ -4,7 +4,7 @@
 from setuptools import setup, find_packages
 setup(
     name='PyDbgEng',
-    version="0.0.4",
+    version="0.0.5",
     description='A python wrapper of debug engines on windows, linux or osx.',
     author='WalkerFuz',
     packages=find_packages(),
